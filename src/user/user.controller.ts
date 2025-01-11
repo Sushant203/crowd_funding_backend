ba9d0cfdac2dd25d0e305661d912f8399a5db861
Sushant <clashofgames229@gmail.com>
2025-01-11 21:19:47 +0545

    chore: add useGuard to get method to make it protective

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
-
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
