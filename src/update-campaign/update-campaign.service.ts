e0e36cf90fe38df8a6d54610b2535245b9731293
Sushant <clashofgames229@gmail.com>
2025-01-13 15:51:48 +0545

    feat: implement CRUD operation for updateCampaign with campaign validation

--- src/update-campaign/update-campaign.service.ts
+++ src/update-campaign/update-campaign.service.ts
@@ -1,26 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUpdateCampaignDto } from './dto/create-update-campaign.dto';
 import { UpdateUpdateCampaignDto } from './dto/update-update-campaign.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UpdateCampaign } from './entities/update-campaign.entity';
+import { Repository } from 'typeorm';
+import { Campaign } from 'src/campaign/entities/campaign.entity';
 
 @Injectable()
 export class UpdateCampaignService {
-  create(createUpdateCampaignDto: CreateUpdateCampaignDto) {
-    return 'This action adds a new updateCampaign';
+  constructor(
+    @InjectRepository(UpdateCampaign)
+    private updateCampaignRepository: Repository<UpdateCampaign>,
+    @InjectRepository(Campaign)
+    private readonly campaignRepository: Repository<Campaign>
+
+  ) { }
+  //create method
+  async create(createUpdateCampaignDto: CreateUpdateCampaignDto): Promise<UpdateCampaign> {
+    const checkCampaign = await this.campaignRepository.findOne({ where: { campaign_id: createUpdateCampaignDto.campaign_id } })
+    if (!checkCampaign) {
+      throw new NotFoundException("campaign not found");
+    }
+
+    const updateCampaignData = this.updateCampaignRepository.create({
+      ...createUpdateCampaignDto,
+      campaign: checkCampaign
+    })
+    return this.updateCampaignRepository.save(updateCampaignData);
   }
 
-  findAll() {
-    return `This action returns all updateCampaign`;
+  // findall method 
+
+  async findAll(): Promise<UpdateCampaign[]> {
+    return this.updateCampaignRepository.find({ relations: ['campaign'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} updateCampaign`;
+  //finding a single update by id 
+  async findOne(update_id: number): Promise<UpdateCampaign> {
+    const checkCampaignUpdate = await this.updateCampaignRepository.findOne({
+      where: { update_id },
+      relations: ['campaign'],
+    });
+    if (!checkCampaignUpdate) {
+      throw new NotFoundException('campaign update not found');
+    }
+    return checkCampaignUpdate;
   }
 
-  update(id: number, updateUpdateCampaignDto: UpdateUpdateCampaignDto) {
-    return `This action updates a #${id} updateCampaign`;
+
+  //update method
+
+  async update(update_id: number, updateUpdateCampaignDto: UpdateUpdateCampaignDto): Promise<UpdateCampaign> {
+    const checkCampaign = await this.campaignRepository.findOne({ where: { campaign_id: updateUpdateCampaignDto.campaign_id } })
+    if (!checkCampaign) {
+      throw new NotFoundException("campaign not found");
+    }
+
+    // Check if the update campaign exists
+    const checkCampaignUpdate = await this.updateCampaignRepository.findOne({
+      where: { update_id },
+      relations: ['campaign'],
+    });
+    if (!checkCampaignUpdate) {
+      throw new NotFoundException('campaign update not found');
+    }
+    // Update the existing entity
+    checkCampaignUpdate.updatedTitle = updateUpdateCampaignDto.updatedTitle;
+    checkCampaignUpdate.updatedContent = updateUpdateCampaignDto.updatedContent;
+    checkCampaignUpdate.campaign = checkCampaign;
+
+    return this.updateCampaignRepository.save(checkCampaignUpdate);
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} updateCampaign`;
+  //remove method
+
+  async remove(update_id: number): Promise<void> {
+    const checkCampaignUpdate = await this.updateCampaignRepository.findOne({ where: { update_id } });
+    if (!checkCampaignUpdate) {
+      throw new NotFoundException('campaign update not found');
+    }
+    await this.updateCampaignRepository.remove(checkCampaignUpdate);
   }
 }
