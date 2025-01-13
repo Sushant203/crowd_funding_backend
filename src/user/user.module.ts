4ab5bfeaf61478679b7e8744f9d8e05c3188b0eb
Sushant <clashofgames229@gmail.com>
2025-01-13 16:32:33 +0545

    feat: add payment entity to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Campaign } from 'src/campaign/entities/campaign.entity';
+import { Payment } from 'src/payment/entities/payment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Campaign])],
+  imports: [TypeOrmModule.forFeature([User, Campaign, Payment])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
