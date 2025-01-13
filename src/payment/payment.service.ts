c6247b74ee929c8818667190fe60e6b22e8db0b9
Sushant <clashofgames229@gmail.com>
2025-01-13 17:15:12 +0545

    feat: implement CRUD operation establishing relation with user and campaign table

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,26 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { Repository } from 'typeorm';
+import { Campaign } from 'src/campaign/entities/campaign.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class PaymentService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
+  constructor(
+    @InjectRepository(Payment)
+    private paymentRepository: Repository<Payment>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Campaign)
+    private campaignRepository: Repository<Campaign>
+
+  ) { }
+  async create(createPaymentDto: CreatePaymentDto): Promise<Payment> {
+    const checkUser = await this.userRepository.findOne({ where: { user_id: createPaymentDto.user_id } });
+    if (!checkUser) {
+      throw new NotFoundException('user not found');
+    }
+
+    const checkCampaign = await this.campaignRepository.findOne({ where: { campaign_id: createPaymentDto.campaign_id } });
+    if (!checkCampaign) {
+      throw new NotFoundException('campaign not found');
+    }
+
+    const paymentData = this.paymentRepository.create({
+      user: checkUser,
+      campaign: checkCampaign,
+      ...createPaymentDto
+    })
+    return this.paymentRepository.save(paymentData);
   }
 
-  findAll() {
-    return `This action returns all payment`;
+  async findAll(): Promise<Payment[]> {
+    return this.paymentRepository.find({ relations: ['user', 'campaign'] })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} payment`;
+  async findOne(payment_id: number): Promise<Payment> {
+    const checkPayment = await this.paymentRepository.findOne({ where: { payment_id }, relations: ['user', 'campaign'] });
+    if (!checkPayment) {
+      throw new NotFoundException('payment not found');
+    }
+    return checkPayment;
   }
 
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
+  async update(payment_id: number, updatePaymentDto: UpdatePaymentDto): Promise<Payment> {
+    const checkUser = await this.userRepository.findOne({ where: { user_id: updatePaymentDto.user_id } });
+    if (!checkUser) {
+      throw new NotFoundException('user not found');
+    }
+
+    const checkCampaign = await this.campaignRepository.findOne({ where: { campaign_id: updatePaymentDto.campaign_id } });
+    if (!checkCampaign) {
+      throw new NotFoundException('campaign not found');
+    }
+
+    const checkPayment = await this.paymentRepository.findOne({ where: { payment_id } });
+    if (!checkPayment) {
+      throw new NotFoundException('payment not found');
+    }
+
+    const updatedPaymentData = this.paymentRepository.create({
+      user: checkUser,
+      campaign: checkCampaign,
+      ...updatePaymentDto
+    })
+
+    return this.paymentRepository.save(updatedPaymentData);
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} payment`;
+  async remove(payment_id: number): Promise<void> {
+    const checkPayment = await this.paymentRepository.findOne({ where: { payment_id } });
+    if (!checkPayment) {
+      throw new NotFoundException('payment not found');
+    }
+    await this.paymentRepository.remove(checkPayment);
   }
 }
