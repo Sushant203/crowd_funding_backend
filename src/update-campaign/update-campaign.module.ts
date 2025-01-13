df2a6c93b715b11034d92f7dcd455f69273af61a
Sushant <clashofgames229@gmail.com>
2025-01-13 13:40:15 +0545

    feat: add updateCampaign and campaign entity to updateCampaign module and updatecampaign entity to app module

--- src/update-campaign/update-campaign.module.ts
+++ src/update-campaign/update-campaign.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UpdateCampaignService } from './update-campaign.service';
 import { UpdateCampaignController } from './update-campaign.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UpdateCampaign } from './entities/update-campaign.entity';
+import { Campaign } from 'src/campaign/entities/campaign.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UpdateCampaign, Campaign])],
   controllers: [UpdateCampaignController],
   providers: [UpdateCampaignService],
 })
-export class UpdateCampaignModule {}
+export class UpdateCampaignModule { }
